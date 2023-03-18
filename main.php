bd8f70d79f4747b130a3041f1b6bef467cfc2392
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-03-18 16:00:06 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,534 @@
+Alfabe
+
+AÆÄBCĆDEFGHİKLMNOÖŒPRŔSTUWXVYZ
+aæåäbcćdefghiklnnprsoöœprŕstuwxvyz
+ 
+Cümle düzeni
+
+ Nesne-Fiil-isim
+
+Nopek zæitezu reitzäit breut heŕ ques'ezu qanz'aa lrw. Öœe qanz'aa lrw Reitzäitet Jelipsta nutgud op'aa. zumeispill Theleapho. Theleapho ogr gut luminio'ezu opkneo. Te'aa Scraneo Lopendentia. Hermune'aa problemiosezu sge aye.
+Ka
+Povene araqan
+Ozak Ćovenratiezu
+Povenesh Kingdem Lrisevia Euoni
+PKLE
+ Al Delufua  nafgt. Lifg juanbeghs Itye parscheftezu
+
+
+Hermune'aa  probelimios sge aye
+
+Det ost Ada. Ada'as ost  kim hermensc.
+
+Wei,ven oks os poven venie nemutania opaa ropedentaa.Esbigalen mone te cratenoui nemutaniezu.Te os nant œnah.habe Te os jv valabel ŕeazvet.
+
+
+Povenesh Karkāstanske det Nbösyrke lkowezu Chine
+
+
+
+Mone'as Lopendentia ööööööö
+Ques dœi
+Mainteoent rerenneren
+Equn Hermensc Falmme
+Joyex Kim gud
+Wei,Mone Alp, Mone hent Tonto. Joyec Hent Pol
+Mone me me'ui sicutelud mew. habe, Loucnutgud mei mei'uip Fhysiquel. Louc 
+
+Wei. Mone Alp. Hent mone tonto. Hent joyec?
+Me me'uo mone sicutelud mew.Habe, Mei mei'uip Loucnutgud Fysiquel . 
+Tohd gut
+Joyec Fhysiquel Sre nutgud 
+
+Joyex ojk äeste ćenvawer martekseru
+Joyex ojk äeste itye dea ćenvarmo martek
+
+Mone'ua Oweloen'as setekenis'k gats liewen'ui
+-Wei
++Wei joyec dœi det povin kingdem
+-Ysateb det Karman
++Oh leoun
+-Kimen'ret?
++Mone Gut,Joyec?
+-Mone Leoun gut
+
+Azumental nost
+
+Ka os jv delufia nafgt. Al nafgt os kamal possala
+Ćveht mone'y Ojk Juhanbegh.Da sgaj Esj Se atolie.Thas Hnosein Kasstchein leoun
+
+Whäur wann Mone'y Hwotsheun ka
+
+Bćadken det Christ Kraœstck
+Bloneädd det Christ Vim
+Thećs det christ nuopman
+
+
+Aile
+
+Anne=Lala
+Baba=Ćama
+Erkek kardeş=Frero
+Kız kardeş=Lomero
+Kardeş=Siuvenia
+
+Soru
+
+Ne=Equn
+Nasıl=Kimen/Hav/Nasılsın=Kimen'ret
+Kim=Klimenre
+Neden=Hew
+Neden(What)=Whäur
+Why=Oks
+Neresi=Dœî
+Kimsin=Ween bass du
+Yardımcı
+To=Mo
+  
+Temel cümleler ve ya Cümle yetine geçen kelimeler
+
+
+Egar dion pusera chava!= iyi günler
+Ogr=Nin,Nım. Kelime arasında kullanılır
+
+
+İşaret 
+Bu=Te
+Şu=Ka
+
+
+
+
+
+
+Sözlük
+
+Wei=Merhaba
+Şamon=Hoşçakal
+geet ot dir=Nasılsın
+Sen=Joyex/ Senin=Joyec
+Siz=Joyex'ye
+Ben=Mone
+Benim=Mone'u
+Biz=Mone'y
+Gel=Duvp'U
+Git=Duvp'I
+Gelme=Duvp'UI
+Gitme=Duvp'IU
+O(dişil)=Khe 
+O(eril)=Ju
+O(Cansız)=Ka
+That=Thas
+İn(ingilizce)=no
+Güzel=Gats
+Çirkin=Tog
+Sadece=Konal
+İs=Os
+İyi=Gud
+Kötü=Nutgud
+En iyi=Venie
+En kötü=Bonua
+Evet=lpa
+Hayır=Nos
+Dönem=Parscheft/erea
+A/An=Al
+Nefis/Leziz=Delufia
+Yaşam,Yaşamak=Liewen(Liven)
+Ölüm,Ölmek=Neweil(Nivel)
+Sevmek/Aşk=Louc
+Nefret=Luoces
+Edebilirim/yapabilirim(Tek)=Gen
+Edebiliriz/yapabiliriz(çoğul)=Lrw
+Ad=Ad
+O=Az
+Onlar=Azper
+Çok=Kim
+Çokluk=Killfalt
+Ama=Habe
+Şey=Ques
+Uzun=Loen
+Dünya=Eqlacq
+Bu=Te
+Şu=Dat
+Öteki=Vraufs
+Beriki=Krenzoft
+Öbürü=den aaneren
+Öbür=Aarwener
+Olmak=Op
+Olmak(geçmiş)=Esj
+So(İng)=Se
+Yıldızlı=atoile
+Parlak=Briœght
+Önem=Oksna
+Gözyaşı=Theć
+Gözyaşları=Thećs
+Kan=Bloneädd
+Her=Breut
+Her zaman=Reitzäit
+Hatırlamak=rerenneren/Hatırladım=arenneren
+Çünkü=Esbigalen
+Şimdi=Mainteoent
+Şeçmek=Aal / Seçtim=Aaaal
+Mal(Hakaret olan)= Hermensc
+The=Du/Das/Des/Dea
+Adam=Falmme
+Baş(Başlangıç)=Ćenva
+Sona=Marteks
+Garip=Toh
+Yaş=Hent
+Kaç=Pol
+Tıpkı=Lifg
+Gençlik=Juanbeghs
+Dinç=Hnosein
+Genç=Juhanbegh
+Hoşlanmak=sicutelud
+Oyun=Mei
+Bebek=Neby
+Ortalama=Oerta
+Ortalama Kişi=Oertavius
+Fizik=Fhysique/Fiziksel=Fhysiquel
+Yap=Ope/Yapmak=Ope'ui
+Geçerli=Valabel
+Sebep=Ŕeazvet
+Uzak=Ozak
+Ordu=Yuhen/Ordusu=Yuhen'aj
+Özgürlük=Bojehmsen
+Yaratmak=Craten
+Yarattım=Cratenoui
+A(ingilizcedeki)=Jv
+Hava=Aore
+Gökyüzü=Sgaj
+Bu yüzden=Bekaŕneye
+Bugün=Ven
+Dün=Ben
+Yarın=Xen
+Sonra=After
+Gün=Jele
+Anlam=Setekenis
+Anlamak=Setekenis'UI
+Anlamı=Setekenis'K
+Malesef=Jelipsta
+From=Itye
+Of=Det
+Eyalet-Ystaten(Yalın) Eğer başka bir kelimeye bağlanıyorsa sonundaki n b olur
+Eyaleti=Ystateb
+Are=Ojk
+Too=leoun
+Yurt=Datlpa
+Yeniden=erëmarrëm
+Yeni=Nopek
+Yönetim=Karkāstanske
+Gece=Naght
+Süre/Zaman=Zæit
+Yok=Azu
+Yokoluş=Azumental
+Oluş=Mental
+Resim=Beln
+Yakın=Nost
+Yaklaşmak=Nost'ui
+Deniz=ßea
+Bilgi=Kraushaft
+Hoş=Angene
+Hoşgeldin=Angeneduvp
+Ülke=Æoul
+İmparatorluk=Enpire
+Ev=Housean
+Kahraman=Āoulq
+Kilise=Oestro
+Kutsal=Holy
+Birlik=Euoni
+Boyamak=Hwotsheun
+Açıklamak=Ropedent
+Yapamazsın/edemesin=Ope'uita
+Gör=Kanzt(Mesul hin halla şehrinde=litaraa)
+Görebilmek=Qanz'aa(MHHŞ=litaraa'ui)
+Tanrı=Tohd
+At(ing)=Et
+Erkek=Fro
+Kadın=Lop
+Soru=Kasstchein
+Mesela=zumeispill
+Özellik=Luminio/Özellikle=Luminionpala(Mesul hin halla şehrinde =miza)
+Gibi=Lam
+Bile=Kneo
+Ekran =Scraneo
+Bağımlılık=Lopendentia
+Bağımsızlık=İndependenta/İndependentium
+İnsan=Hermuno/Pealpe
+Çoğunluk sahip olma=Sge
+Problem/sorun=Problemios
+İstihbarat=Lrisevia 
+Kusmak=Ööööööö(eski)
+Kusmak=Vremgiso(yeni)
+Dil=Nemutania
+Merkez=Kentral
+Orta=Mikkle
+Çin=Chine
+Liman=Lkow
+Kıyı=Nbösyrke
+Bina=Äotmen
+Kolon=Vaksviyaret
+Vücut=Bćadken
+Vinç=Krän
+Taç=Krāwn
+Dolap=Quksped
+Yer=Ćovenrat
+Toprak=Azak
+Haklı=Äeste
+Alarm=Alerj
+Acil=Erghestrimanigen
+İnanılmaz!=Rahbirgenden!
+İyi(2.Yol)=Egar Dion
+Günler=Pusera Chava
+Video=Video
+Liste=Listæ
+Mümkün=possala
+Kaprisli=Capreous
+Yapabilmek=Cauls
+Alışveriş=Ćhohhaa
+Yokolmak=Vćhanenaa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Bazı nesneler ve canlılar
+
+
+İnsan=Jophen
+Telefon=Theleapho(Telayfo diye okunur)
+Kapı=Xāve
+Elma=Æple
+Armut=Pėar
+Üzüm=Drauwe
+Portakal=Orejs
+Domates=Tomestos
+Salatalık=Bkesspo
+Zeytin=Olive
+Çikolata=Chocosta(Afrikuen)
+Masa=Table
+Yağ=Egeas
+
+
+Unvanlar
+
+
+Sayın=Efiüak
+Majesteleri= Makshevetea
+Majeste=Makhseulio
+Ekseleansları=Ekselantiaa
+Ekselans=Ekselantes
+Bay=Fufyah
+Bayan=Ćevelactia
+
+
+
+
+
+Matematiksel İfadeler,
+ Artı=Peush
+Eksi=Nermus
+Toplama=Dje
+Çarpma=Stinugen
+
+
+
+
+
+
+
+Sayılar
+Bir=To
+İki=Sko
+Üc=Tois
+Dört=Nahem
+Beş=Pen
+Altı=Fon
+Yedi=Feurg
+Sekiz=Vas
+Dokuz=Naskettin
+On=Ton
+Yirmi=Tonn
+Otuz=Tonnn
+Kırk=Tonk
+
+Duygular
+
+Korku=Hofor
+Mutlu=Happe
+Üzgün=Dgestru
+Şaşkın=Voleantia
+
+
+
+
+
+İğrenme=Langeniki
+İğrenç=Distanges
+
+
+
+
+Ekler
+
+De/Da=Nes
+Ler/lar=Azu/Ezu
+Mastar=UI,Aa
+İyelik=as/es/os
+Ce=ua
+Sın=Ret
+Ten/Tan=Wer
+
+
+
+
+
+Sorusal=
+Eğer=wann
+Fakat=Öœe 
+
+Renkler
+
+Mavi=Bleo
+Kırmızı=Redell
+Yeşil=Œess
+Sarı=Yelloc
+Turuncu=Orenge
+Mor=mauve
+Pembe=Pink
+
+
+
+
+
+Şekiller
+
+
+Kare=Quadrat
+Yuvarlak=Ronn
+Ücgen=Drieck
+Dikdörtgen=Rechteck
+Beşgen=Penop
+Altıgen=Hozogo
+Yedigen=Hetygo
+Sekizgen=Oktago
+Daha çok kenarı olan şekillerde başa kenar sayısına Chon denir
+Prizmalarda Sona geomic eklenir
+
+
+3D
+
+Küp=Kubes
+Küre/Top=Alsen
+
+
+Filler
+
+
+Koşmak=Gemis'UI
+Yürümek=Łun'UI
+Kalkmak=Mines'UI
+İnmek=Sopu'UI
+Gel=Duvp'U
+Git=Duvp'I
+Gelme=Duvp'UI
+Gitme=Duvp'IU
+Gitmek=Duvp'UIO
+Gelmek=Duvp'OIU/Gelmek(1.şahıs)=Duvp'oiu mew
+Olmak=Op
+Şeçmek=Aal / Seçtim=Aaal
+Oyna=Mei/Oynamak=Mei'UI
+Bırak=ZrÎt'ui
+Koru=litah'ui
+Sallamak=Cratleve'ui
+Düşünmek=Teschupt
+Görmek/Bakmak=laaf/sing
+
+
+
+
+
+
+
+
+
+
+Irklar
+
+Povin=Poven
+Fransız=Franćaksevio
+Alman=Gerbæcth
+Türk=Turken
+İngiliz=Anglidian
+Arap=Araqion 
+
+
+
+
+
+
+
+
+
+
+ 
+Yönler
+
+Sağ=Greut
+Sol=Vol
+Ön=Nretu
+Arka=Behils
+
+Kuzey=Nortske
+Güney=Sud
+Doğu=Estrean
+Batı=Jkellos
+Kuzeydoğu= Nortestrean
+Kuzeybatı=Nortskekollos
+Güneydoğu=Sudestran
+Güneybatı=Sudakellos
+
+
+
+
+
+
+Kıtalar
+
+Afrika=Afrika(Afrikalı=Afrikanen)
+Amerika=Amercika (Amerikalı=Amercikanien)
+Avrupa=Eurapa (Avrupalı=Eurapanian)
+Asya=Asea(Asyalı=Asyenikaen)
+Avustralya=Austrelia(Sudestromanien)
+Antartika=Antertika(Anternikatenian)
+Astersudemanromanikatenianterrailikaen
+Astersudemanromanikatenianterrailikaenidanit-onplabeniumlumiopalastion
+
+Vücut parçaları
+
+
+Göz=Aye
+
+
+
+
+
+
+Zaman
+
+When=Ćveht
+Saat=Heŕ
+
+
+Bilgi
+Mew, kendisinden önce gelen kelimeyle 1.şahısı ilişkilendirir
+Ch hecesi Y diye okunur
